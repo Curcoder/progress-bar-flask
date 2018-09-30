b51565a8016037cf6e0fcd7fea1529159640235a
curcoder <curcode@hotmail.com>
2018-09-30 12:25:42 -0400

    Added version 1.1

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def home():
+    bar_data = {'HTML': 95, 'CSS': 90, 'Javascript': 40, 'Python': 75, 'Flask': 60}
+    return render_template('progressbars.html', bar_data=bar_data)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
